eff2fad0088074936a1aff0e80cc87947e50c766
wangbin <wangbin@inovance.com>
2021-11-12 14:14:19 +0800

    修改依赖关系，如下： hwapp1  hwapp2    \      /     \    /     hwdll

--- cmake demo1/hwapp1/main.cpp
+++ cmake demo1/hwapp1/main.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-	print();
+    print();
     getchar();
 
     return 0;
---
+++ cmake demo1/hwapp2/main.cpp
@@ -0,0 +1,11 @@
+#include "helloworld.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    print();
+    getchar();
+
+    return 0;
+}
