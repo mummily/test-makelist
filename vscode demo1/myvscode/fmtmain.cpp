35935f8bb203349f8cffd1f7622c9ba7458c1890
wangbin <wangbin@inovance.com>
2021-11-15 13:51:22 +0800

    fmt库的使用

---
+++ vscode demo1/myvscode/fmtmain.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include "fmt\format.h"
+#include <string>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    std::string str1 = fmt::format("Hello, {}!", "world");
+    std::cout << str1 << std::endl;
+
+    fmt::print("Hello, {}!", "world");
+}
