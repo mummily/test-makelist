9a0d8424d44cb17b09ea57743d39b6d5641bd96d
wangbin <wangbin@inovance.com>
2021-11-19 19:04:28 +0800

    添加operator->的使用

---
+++ vscode demo1/myvscode/operatorptrmain.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class A
+{
+public:
+    void display() { cout << "A" << endl; }
+};
+
+class B
+{
+public:
+    B(A &ra) : ptr(&ra) {}
+    A *operator->() { return ptr; }
+    
+    void display() { cout << "B" << endl; }
+
+private:
+    A *ptr;
+};
+
+// 结论：->重载根据栈对象，若为堆对象，只能是自己的->
+int main()
+{
+    A a;
+    B b(a);
+    b->display(); // A
+
+    B *pB = new B(a);
+    pB->display();    // B
+    (*pB)->display(); // A
+
+    return 0;
+}
