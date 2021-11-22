b149da134e98a7dd105a826dcff5941cfc0a9806
wangbin <wangbin@inovance.com>
2021-11-22 10:12:34 +0800

    测试fmt十六进制显示

--- consoleappliaction/fmtmain.cpp
+++ consoleappliaction/fmtmain.cpp
@@ -8,5 +8,9 @@ int main(int argc, char **argv)
     std::string str1 = fmt::format("Hello, {}!", "world");
     std::cout << str1 << std::endl;
 
-    fmt::print("Hello, {}!", "world");
+    fmt::print("Hello, {}!\n", "world");
+
+    int i = 2147483647;
+    printf("value=0x%lx\n", i);
+    fmt::print("value=0x{:x}", i);
 }
